5f84dc68e55bf94df9b2b65215d6710aea837018
jonathan patelski <Jonathanpatelski@gmail.com>
2023-09-06 12:08:49 +0200

    feat: Updated dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <header>
+        <nav class="bg-indigo-950 border-gray-200">
+            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                <a href="" class="flex items-center">
+                    <img src="" class="h-8 mr-3" alt="Delos Logo" />
+                    <span class="self-center text-2xl text-white font-semibold whitespace-nowrap">Delos</span>
+                </a>
+                <div class="flex items-center">
+                    <a href="#" class="text-sm  text-blue-600 hover:underline">Login</a>
+                </div>
+            </div>
+        </nav>
+        <nav class="bg-slate-950">
+            <div class="max-w-screen-xl px-4 py-3 mx-auto">
+                <div class="flex items-center">
+                    <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+                        <li>
+                            <a href="#" class="text-white hover:underline" aria-current="page">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-white hover:underline">Incidents</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-white hover:underline">Crews</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-white hover:underline">Hovercraft</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div class="w-[1600px] h-[1000px] relative bg-stone-50">
+        <div class="w-[448px] h-[172px] left-[1117px] top-[484px] absolute">
+            <div class="w-[448px] h-[172px] left-0 top-0 absolute justify-center items-center inline-flex">
+                <div class="w-[448px] h-[172px] bg-white rounded-[10px]"></div>
+            </div>
+            <div class="w-[369px] h-[81px] left-[30px] top-[80px] absolute">
+                <div class="w-[357.67px] h-3 left-0 top-[71px] absolute">
+                    <div class="w-[357.67px] h-2 left-0 top-[2px] absolute justify-center items-center inline-flex"></div>
+                    <div class="w-[230px] h-3 left-0 top-0 absolute"></div>
+                </div>
+                <div class="left-[3px] top-[40px] absolute text-black text-xl font-bold leading-tight">Severity level :</div>
+            </div>
+            <div class="left-[30px] top-[10px] absolute text-black text-xl font-bold leading-tight">Incident type :</div>
+        </div>
+        <div class="w-[448px] h-[172px] left-[1117px] top-[238px] absolute">
+            <div class="w-[448px] h-[172px] left-0 top-0 absolute justify-center items-center inline-flex">
+                <div class="w-[448px] h-[172px] bg-white rounded-[10px]"></div>
+            </div>
+            <div class="w-[369px] h-[81px] left-[30px] top-[80px] absolute">
+                <div class="w-[357.67px] h-3 left-0 top-[71px] absolute">
+                    <div class="w-[357.67px] h-2 left-0 top-[2px] absolute justify-center items-center inline-flex"></div>
+                    <div class="w-[298px] h-3 left-0 top-0 absolute justify-center items-center inline-flex"></div>
+                </div>
+                <div class="left-[3px] top-[40px] absolute text-black text-xl font-bold leading-tight">Severity level :</div>
+            </div>
+            <div class="left-[30px] top-[10px] absolute text-black text-xl font-bold leading-tight">Incident type :</div>
+        </div>
+        <div class="left-[1313px] top-[301px] absolute text-black text-xl font-bold leading-tight">Low beach Casino</div>
+        <div class="w-[214px] left-[1147px] top-[284px] absolute text-black text-xl font-bold leading-tight">Near points of interest :</div>
+        <div class="left-[1313px] top-[248px] absolute text-black text-xl font-bold leading-tight">Robot malfunction</div>
+        <div class="left-[1313px] top-[547px] absolute text-black text-xl font-bold leading-tight">Royal Bear Inn</div>
+        <div class="w-[214px] left-[1147px] top-[530px] absolute text-black text-xl font-bold leading-tight">Near points of interest :</div>
+        <div class="w-[204px] h-11 left-[1356px] top-[26px] absolute">
+            <div class="w-11 h-11 left-[106px] top-0 absolute justify-center items-center inline-flex"></div>
+            <div class="w-11 h-11 left-[53px] top-0 absolute justify-center items-center inline-flex"></div>
+            <div class="w-11 h-11 left-0 top-0 absolute justify-center items-center inline-flex"></div>
+            <div class="w-11 h-11 left-[160px] top-0 absolute"></div>
+            <div class="w-3 h-3 left-[32px] top-[32px] absolute"></div>
+            <div class="w-6 h-6 left-[170px] top-[10px] absolute"></div>
+        </div>
+        <img class="w-[850px] h-[750px] left-[213px] top-[51px] absolute" src="https://via.placeholder.com/850x850" />
+        <div class="w-[443px] h-[138px] left-[1117px] top-[142px] absolute text-black text-[50px] font-bold">Incident reports</div>
+        <div class="left-[1313px] top-[494px] absolute text-black text-xl font-bold leading-tight">Gas station explosion</div>
+        <div class="w-[448px] h-[172px] left-[1117px] top-[740px] absolute">
+            <div class="w-[448px] h-[172px] left-0 top-0 absolute justify-center items-center inline-flex">
+                <div class="w-[448px] h-[172px] bg-white rounded-[10px]"></div>
+            </div>
+            <div class="w-[369px] h-[81px] left-[30px] top-[80px] absolute">
+                <div class="w-[357.67px] h-3 left-0 top-[71px] absolute">
+                    <div class="w-[357.67px] h-2 left-0 top-[2px] absolute justify-center items-center inline-flex"></div>
+                    <div class="w-[196px] h-3 left-0 top-0 absolute"></div>
+                </div>
+                <div class="left-[3px] top-[40px] absolute text-black text-xl font-bold leading-tight">Severity level :</div>
+            </div>
+            <div class="left-[30px] top-[10px] absolute text-black text-xl font-bold leading-tight">Incident type :</div>
+        </div>
+        <div class="left-[1313px] top-[803px] absolute text-black text-xl font-bold leading-tight">Golden Coffee Shop</div>
+        <div class="w-[214px] left-[1147px] top-[786px] absolute text-black text-xl font-bold leading-tight">Near points of interest :</div>
+        <div class="left-[1313px] top-[750px] absolute text-black text-xl font-bold leading-tight">Robot massacre</div>
+    </div>
+</body>
+
+</html>
