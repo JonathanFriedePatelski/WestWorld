f157b1756bf1f0eaa3910bc3ee0cb5a1aeb75996
wesselwillemsen <wessolwillemsen@outlook.com>
2023-09-07 13:05:56 +0200

    fix: too many placeholders

--- database/seeders/IncidentSeeder.php
+++ database/seeders/IncidentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use App\Models\Incident;
 use App\Models\PointOfInterest;
 use Illuminate\Database\Seeder;
@@ -17,18 +18,26 @@ class IncidentSeeder extends Seeder
     public function run()
     {
         $poiIds = PointOfInterest::pluck('id', 'title')->all();
-
-        foreach (Storage::json('data/incident_reports.json') as $incident) {
-            $newIncidents[] = [
-                'point_of_interest_id' => $poiIds[$incident['point_of_interest']],
-                'occurred_at' => $incident['timestamp'],
-                'latitude' => $incident['latitude'],
-                'longitude' => $incident['longitude'],
-                'type' => $incident['type'],
-                'severity' => $incident['severity'],
-            ];
+        $allIncidents = Storage::json('data/incident_reports.json');
+    
+        try {
+            foreach (array_chunk($allIncidents, 500) as $incidentChunk) {  // Reduced chunk size to 500
+                $newIncidents = [];
+                foreach ($incidentChunk as $incident) {
+                    $newIncidents[] = [
+                        'point_of_interest_id' => $poiIds[$incident['point_of_interest']],
+                        'occurred_at' => $incident['timestamp'],
+                        'latitude' => $incident['latitude'],
+                        'longitude' => $incident['longitude'],
+                        'type' => $incident['type'],
+                        'severity' => $incident['severity'],
+                    ];
+                }
+                Incident::upsert($newIncidents, ['latitude', 'longitude']);
+            }
+        } catch (\Exception $e) {
+            throw $e;
         }
-
-        Incident::upsert($newIncidents, ['latitude', 'longitude']);
     }
+    
 }
