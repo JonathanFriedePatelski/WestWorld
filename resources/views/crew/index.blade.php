145e325effb723553a473348680d38110881213e
Raymon Roos <raymon.roos@hotmail.com>
2023-09-08 09:51:49 +0200

    fix(views): implement linkes in Bart's crew index page

--- resources/views/crew/index.blade.php
+++ resources/views/crew/index.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
-    <div class="bg-white py-24 sm:py-32 border-red-700 border-2">
+    <div class="bg-gray-800 py-24 sm:py-32">
         <div class="mx-auto grid max-w-4xl gap-x-8 gap-y-16 px-4 sm:px-6 lg:px-8 xl:grid-cols-3">
             @foreach($crews as $crew)
-            <ul role="list" class="grid border border-gray-300  rounded-[120rem]  shaow-md shadow-lg gap-8 sm:grid-cols-2">
-                <li class=p-4 rounded-lg w-fit hover:bg-gray-950 cursor-pointer" onclick="window.location.href='{{ route("crews.show", $crew->id) }}'">
+            <ul role="list" class="grid border border-gray-300 hover:bg-gray-950 cursor-pointer rounded-[120rem]  shadow-md gap-8 sm:grid-cols-2" onclick="window.location.href='{{ route("crews.show", $crew->id) }}'">
+                <li class="p-4 rounded-lg w-fit">
                     <div class="flex items-center gap-4 w">
                         <img class="h-16 w-16 rounded-full" src="https://picsum.photos/200" alt="{{ $crew->call_sign }}">
                         <div class="flex flex-col">
-                            <h3 class="text-lg font-semibold text-gray-900">{{ $crew->call_sign }}</h3>
+                            <h3 class="text-lg font-semibold text-indigo-300">{{ $crew->call_sign }}</h3>
                         </div>
                     </div>
                 </li>
