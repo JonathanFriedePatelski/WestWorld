411043f219abffb655618778fa7e22fd1909b3b0
jonathan patelski <Jonathanpatelski@gmail.com>
2023-09-19 10:14:50 +0200

    feat: added border details to incident cards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,14 +39,14 @@
             </div>
         </div>
 
-        <div class="flex flex-col space-y-4 w-2/3 md:w-1/2">
+        <div class="flex flex-col space-x-7 w-2/3 md:w-1/2">
             <div class="flex flex-col space-y-4 w-2/3 md:w-1/2">
                 <p class="drop-shadow-[1.2px_rgba(0,0,0,0.8)] ml-9 mt-5 text-blue-700 text-2xl font-bold">
-                    Incident reports
+                    Unclaimed Incidents
                 </p>
 
                 @foreach(App\Models\Incident::orderByDesc('occurred_at')->with('pointOfInterest:id,title')->limit(6)->get() as $incident)
-                <div class="border rounded-md p-4 ml-9 mt-5">
+                <div class="border-teal-800 rounded-md p-4 ml-9 mt-5 bg-black shadow-gray-600 hover:shadow-indigo-500/50 hover:shadow-2xl shadow-lg hover:border-8">
                     <p class="text-blue-600 text-xl font-bold leading-tight">
                         Located near: {{ $incident->pointOfInterest->title }}
                     </p>
